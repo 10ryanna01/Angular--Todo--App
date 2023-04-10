ce98a629d4a8b62ffeeebbb2fafabe562ed01a40
Ryann Al-lahiq <ryann.allahiq@gmail.com>
2023-04-10 19:56:41 +0100

    :rocket: deploy update

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,37 +1,37 @@
 import { Component, OnInit } from "@angular/core";
 import { Todo } from "./../../models/Todo";
-import { FormBuilder ,FormGroup, FormGroupDirective, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup, FormGroupDirective, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-todos",
   templateUrl: "./todos.component.html",
   styleUrls: ["./_todos.component.scss"],
-  
+
 })
 export class TodosComponent implements OnInit {
   title = " title template litteral todos";
 
   todos: Todo[] = [];
-  inputTodo: string = ""; 
-  formDirective!:FormGroupDirective
-  registerForm!:FormGroup 
-  submitted =false;
+  inputTodo: string = "";
+  formDirective!: FormGroupDirective
+  registerForm!: FormGroup
+  submitted = false;
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     // todo validation
     this.registerForm = this.formBuilder.group({
-    firstName:['', Validators.required]
-      
-    },{  updateOn: 'submit' })
+      firstName: ['', Validators.required]
+
+    }, { updateOn: 'submit' })
 
     console.log("OnInit");
     this.todos = [
       {
         content: "create git repo",
         completed: true,
-      },{
+      }, {
         content: "add files to commit",
         completed: false,
       },
@@ -53,28 +53,28 @@ export class TodosComponent implements OnInit {
 
   addTodo() {
     this.submitted = true;
-    if(this.registerForm.invalid ){ 
+    if (this.registerForm.invalid) {
       return;
-      
-    } 
+
+    }
     this.todos.push({
       content: this.inputTodo,
       completed: false,
-      
-    }); 
 
-   
-    
-     }
+    });
 
- 
+
+
+  }
+
+
 
   deleteTodo(id: number) {
     this.todos = this.todos.filter((v, i) => i !== id);
   }
 
- 
 
- 
- 
+
+
+
 }
